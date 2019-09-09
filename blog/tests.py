d1114137c4649ba00ede45774167b98ab9afa1be
askvart <askvart@gmail.com>
2019-09-09 16:10:51 +0300

    шестая глава

--- blog/tests.py
+++ blog/tests.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Post
 
-
 class BlogTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
@@ -12,6 +11,7 @@ class BlogTests(TestCase):
             email='test@email.com',
             password='secret'
         )
+
         self.post = Post.objects.create(
             title='A good title',
             body='Nice body content',
@@ -21,6 +21,9 @@ class BlogTests(TestCase):
     def test_string_representation(self):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
@@ -40,3 +43,26 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self): # new
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(
+        reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
